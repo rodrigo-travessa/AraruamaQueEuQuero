f97a2926280a5fa970c66fc02cc2e4ee0de13e72
RodrigoTravessa <rodrigosigiliao@outlook.com>
2023-12-12 14:34:46 -0300

    new site

---
+++ AraruamaQueEuQuero/Pages/Projeto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AraruamaQueEuQuero.Pages
+{
+    public class ProjetoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
